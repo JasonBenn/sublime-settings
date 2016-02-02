94b9e4cf58dd4f2644ee6d06b6cf41d0848c62b9
Jason Benn <jasoncbenn@gmail.com>
2016-02-02 14:03:19 -0800

    Install script.

---
+++ install.sh
@@ -0,0 +1,5 @@
+user_settings_dir="/Users/jasonbenn/Library/Application Support/Sublime Text 3/Packages/User"
+
+ls -1 | egrep -v install.sh | while read x; do
+  ln -sf "$pwd/$x" "$user_settings_dir/$x"
+done
